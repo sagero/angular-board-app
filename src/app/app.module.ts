68c4bb7fbd36ef3fee345717a57303e0ad7e25a1
Siarhey Kiniou <netralitet@gmail.com>
2019-09-26 11:29:49 +0300

    feat: generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { BoardComponent } from './components/board/board.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToolbarComponent,
+    BoardComponent,
+    FooterComponent,
+    CardListComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule,
